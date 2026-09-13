dc51a5bad51953ec501523b241d5d86f31c67718
khanif13 <ahmadhanif13125@gmail.com>
2026-09-13 23:40:34 +0800

    first commit

---
+++ app/Livewire/Quizzes/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Quizzes;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.quizzes.index');
+    }
+}
